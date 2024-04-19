3c908384ef1806ed4c31831187469629e397cd00
VPPI\richard.puig <richard.puig@veritaspay.com>
2024-04-19 15:37:16 +0800

    added validators to sign in

--- src/Web/ClientApp-Angular/alfamal/src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/Web/ClientApp-Angular/alfamal/src/app/modules/auth/sign-in/sign-in.component.ts
@@ -5,11 +5,12 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClientService } from '../../../core/services/http-client.service';
 import { UserCredential } from '../../../core/models/user-credential';
 import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule, CommonModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
@@ -20,30 +21,23 @@ export class SignInComponent implements OnInit, AfterViewInit {
 
   constructor(
     private fb: FormBuilder,
-    private httpService: HttpClientService,
     private authService: AuthService) { }
 
   signInForm = this.fb.group({
-    username: [''],
-    password: ['']
+    username: ['', [Validators.required, Validators.minLength(8), Validators.max(25)]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).+$')]]
   });
 
   onSubmit() {
+    if (this.signInForm.invalid) {
+      this.signInForm.markAllAsTouched();
+      return;
+    }
     const userCredential = this.signInForm.value as UserCredential;
     this.authService.login(userCredential)
       .subscribe(res => {
         console.log(res);
       })
-
-    // const res = this.httpService.getData('api/product')
-    //   .subscribe(data => {
-    //     console.log(data);
-    //   })
-
-    // const res = this.httpService.getDataById('api/product', '9874dca7-992a-414c-9a70-e4ed8c0f610f')
-    //   .subscribe(data => {
-    //     console.log(data);
-    //   })
   }
 
   ngOnInit() {
